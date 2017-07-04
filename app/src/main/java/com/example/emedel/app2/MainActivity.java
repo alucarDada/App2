70c40f0f9fb882a47326e190c7e02bb1ae0f5c96
alucarDada <alucard0cronwell@gmail.com>
2017-07-04 14:08:25 -0500

    ListView una lista Scrollable

--- app/src/main/java/com/example/emedel/app2/MainActivity.java
+++ app/src/main/java/com/example/emedel/app2/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.emedel.app2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        List<String> names = new ArrayList<String>();
+        names.add("A");
+        names.add("B");
+        names.add("C");
+        names.add("D");
+        names.add("E");
+        names.add("A");
+        names.add("B");
+        names.add("C");
+        names.add("D");
+        names.add("E");
+        names.add("A");
+        names.add("B");
+        names.add("C");
+        names.add("D");
+        names.add("E");
+        names.add("A");
+        names.add("B");
+        names.add("C");
+        names.add("D");
+        names.add("E");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, names);
+
+        listView.setAdapter(adapter);
+
     }
 }
